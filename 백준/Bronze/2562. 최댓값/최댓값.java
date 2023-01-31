06cd16312215e67c0fdab2fbb7924b5bb8abf626
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-01-31 22:37:53 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int index = 0;
+        
+        for (int i = 1; i <= 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > max) {
+                max = num;
+                index = i;
+            }
+        }
+        System.out.print(max + "\n" + index);
+    }
+}
