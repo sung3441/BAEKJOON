a05004f05ecba90116c6929951ce4b7f8907be2a
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-05 23:24:07 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 1.58 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,14 +1,10 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(String t, String p) {
-        int result = 0;
-        int len = p.length();
-        long num = Long.parseLong(p);
-        
-        for (int i = 0; i <= t.length() - len; i++) {
-            if (num >= Long.parseLong(t.substring(i, i + len))) {
-                result++;
-            }
-        }
-        return result;
+        return (int) IntStream.range(0, t.length() - p.length() + 1)
+                .mapToObj(i -> Long.parseLong(t.substring(i, i + p.length())))
+                .filter(i -> Long.parseLong(p) >= i)
+                .count();
     }
 }
