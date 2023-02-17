fee72790d18c30213f40e3046ab6a3ce52777f06
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-17 13:40:56 +0900

    [Silver V] Title: 소수 찾기, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int cnt = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num > 1 && isSosu(num)) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+
+    static boolean isSosu(int num) {
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
