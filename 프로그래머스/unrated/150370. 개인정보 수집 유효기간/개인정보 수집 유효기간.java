178dfa4b9d2c142f0d15ab917dbba21a1e88c235
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-19 20:16:27 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 13.36 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -5,80 +5,54 @@ import java.util.HashMap;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         ArrayList<Integer> result = new ArrayList<>();
-        Terms.saveTerms(terms);
+        HashMap<String, Integer> termsStore = new HashMap<>();
+
+        for (String term : terms) {
+            termsStore.put(term.split(" ")[0], Integer.parseInt(term.split(" ")[1]));
+        }
 
         for (int i = 0; i < privacies.length; i++) {
             String createdDate = privacies[i].split(" ")[0];
             String termsName = privacies[i].split(" ")[1];
-            Terms term = Terms.termsStore.get(termsName);
-            if (term.isExpired(createdDate, today)) {
+            if (isExpired(createdDate, today, termsStore.get(termsName))) {
                 result.add(i + 1);
             }
         }
         return result.stream().mapToInt(i -> i).toArray();
     }
-}
-
-class Terms {
-    static HashMap<String, Terms> termsStore = new HashMap<>();
-
-    String name;
-    int expiredMonth;
-
-    public Terms(String name, int expiredMonth) {
-        this.name = name;
-        this.expiredMonth = expiredMonth;
-    }
-
-    boolean isExpired(String createdDate, String today) {
-        Date expiredDate = new CustomDate(createdDate).calDate(expiredMonth);
-        Date todayDate = CustomDate.toDate(today);
+    
+    static boolean isExpired(String createdDate, String today, int expiredMonth) {
+        Date todayDate = toDate(today);
+        Date expiredDate = calDate(createdDate, expiredMonth);
         return todayDate.compareTo(expiredDate) > 0;
     }
 
-    static void saveTerms(String[] termsArr) {
-        for (String term : termsArr) {
-            String name = term.split(" ")[0];
-            int expiredMonth = Integer.parseInt(term.split(" ")[1]);
-            Terms terms = new Terms(name, expiredMonth);
-            termsStore.put(terms.name, terms);
-        }
-    }
-}
+    static Date calDate(String createDate, int expiredMonth) {
+        String[] split = createDate.split("\\.");
+        int yy = Integer.parseInt(split[0]);
+        int mm = Integer.parseInt(split[1]);
+        int dd = Integer.parseInt(split[2]);
 
-class CustomDate {
-    int yy;
-    int mm;
-    int dd;
-
-    static Date toDate(String date) {
-        String[] split = date.split("\\.");
-        return new Date(Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]));
-    }
-
-    public CustomDate(String date) {
-        String[] split = date.split("\\.");
-        yy = Integer.parseInt(split[0]);
-        mm = Integer.parseInt(split[1]);
-        dd = Integer.parseInt(split[2]);
-    }
-
-    Date calDate(int month) {
         if (dd == 1) {
             dd = 29;
-            month--;
+            expiredMonth--;
         }
         dd--;
-        if (month / 12 > 0) {
-            yy += month / 12;
-            month %= 12;
+        if (expiredMonth / 12 > 0) {
+            yy += expiredMonth / 12;
+            expiredMonth %= 12;
         }
-        if (mm + month > 12) {
+        if (mm + expiredMonth > 12) {
             yy++;
-            mm += month - 12;
-            month = 0;
+            mm += expiredMonth - 12;
+            expiredMonth = 0;
         }
-        mm += month;
+        mm += expiredMonth;
         return new Date(yy, mm, dd);
+    }
+
+    static Date toDate(String date) {
+        String[] arr = date.split("\\.");
+        return new Date(Integer.parseInt(arr[0]), Integer.parseInt(arr[1]), Integer.parseInt(arr[2]));
     }
 }
