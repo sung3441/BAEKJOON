f385b845603adbd07ea38f0e12f1ef5970e29196
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-21 10:16:13 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -18,7 +18,7 @@ public class Main {
     }
 
     static boolean isSosu(int num) {
-        for (int i = 2; i <= num / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
