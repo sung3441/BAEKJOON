2e04f77deb6c8cab55937557d5deb78a36dbbff3
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-22 10:52:01 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        for (int i = 2; i < n / 2; i++) {
+        for (int i = 2; i * i <= n; i++) {
             while (n % i == 0) {
                 n /= i;
                 System.out.println(i);
