ed3803f6292873761bfe49dd7867c96a5bf4ccba
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-23 17:03:17 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public interface Main{
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
