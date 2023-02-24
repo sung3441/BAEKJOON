cba4150cdbc3c1fee1b3971bb8c4a6cca4c3ca54
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-24 09:32:12 +0900

    [Bronze I] Title: 약수들의 합, Time: 148 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,31 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         String num = br.readLine();
 
         while (!"-1".equals(num)) {
-            System.out.println(solution(Integer.parseInt(num)));
+            sb.append(solution(Integer.parseInt(num)));
             num = br.readLine();
         }
+        System.out.println(sb);
     }
 
     static String solution(int num) {
-        int sum = 1;
         StringBuilder sb = new StringBuilder();
-        sb.append(num + " = 1");
-
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                sum += i;
-                if (sum > num) {
-                    return num + " is NOT perfect.";
-                }
-                sb.append(" + " + i);
-            }
+        sb.append(num);
+        int[] arr = IntStream.range(1, num).filter(i -> num % i == 0).toArray();
+        if (num != Arrays.stream(arr).sum()) {
+            return sb.append(" is NOT perfect.\n").toString();
         }
-        return num == sum ? sb.toString() : num + " is NOT perfect.";
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(i == 0 ? " = " : " + ");
+            sb.append(arr[i]);
+        }
+        return sb.append("\n").toString();
     }
 }
