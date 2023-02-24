82b329325723ddf7bad08ba98551aa1954bd9476
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-02-24 09:36:06 +0900

    [Bronze I] Title: 약수들의 합, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -6,27 +6,23 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         String num = br.readLine();
-
         while (!"-1".equals(num)) {
-            sb.append(solution(Integer.parseInt(num)));
+            System.out.println(solution(Integer.parseInt(num)));
             num = br.readLine();
         }
-        System.out.println(sb);
     }
 
-    static String solution(int num) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(num);
+    static StringBuilder solution(int num) {
+        StringBuilder sb = new StringBuilder(String.valueOf(num));
         int[] arr = IntStream.range(1, num).filter(i -> num % i == 0).toArray();
         if (num != Arrays.stream(arr).sum()) {
-            return sb.append(" is NOT perfect.\n").toString();
+            return sb.append(" is NOT perfect.");
         }
         for (int i = 0; i < arr.length; i++) {
             sb.append(i == 0 ? " = " : " + ");
             sb.append(arr[i]);
         }
-        return sb.append("\n").toString();
+        return sb;
     }
 }
