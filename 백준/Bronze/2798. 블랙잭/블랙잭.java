3bbb34a31840a8ce6ddb6a0e322705bafb2bff7c
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-03-14 10:12:24 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+        int[] arr = new int[n];
+        int max = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= m && sum > max) {
+                        max = sum;
+                    }
+                }
+            }
+        }
+        System.out.print(max);
+    }
+}
