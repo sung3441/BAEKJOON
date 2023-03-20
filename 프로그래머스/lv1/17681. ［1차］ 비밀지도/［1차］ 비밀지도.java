9d6444c42a267b16fb1ea4ca931d2447cf0485e0
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-03-20 23:11:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,22 +1,12 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
-        String format = "%0" + n + "s";
         for (int i = 0; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            String s1 = Integer.toBinaryString(arr1[i]);
-            String s2 = Integer.toBinaryString(arr2[i]);
-            String[] split1 = ("0".repeat(n - s1.length()) + s1).split("");
-            String[] split2 = ("0".repeat(n - s2.length()) + s2).split("");
-            
-            for (int j = 0; j < n; j++) {
-                if (split1[j].equals("0") && split2[j].equals("0")) {
-                    sb.append(" ");
-                    continue;
-                }
-                sb.append("#");
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
+            if (n > binary.length()) {
+                binary = "0".repeat(n - binary.length()) + binary;
             }
-            result[i] = sb.toString();
+            result[i] = binary.replace("0", " ").replace("1", "#");
         }
         return result;
     }
