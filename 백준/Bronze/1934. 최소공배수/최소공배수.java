d2ea2d2ffbd3ed309fe0e7a54dc0a33bc9f8f2f6
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-03-21 13:49:11 +0900

    [Bronze I] Title: 최소공배수, Time: 292 ms, Memory: 15240 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -4,24 +4,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             String[] split = br.readLine().split(" ");
-            int result = solution(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
-            System.out.println(result);
+            sb.append(solution(Integer.parseInt(split[0]), Integer.parseInt(split[1]))).append("\n");
         }
+        System.out.print(sb);
     }
     
     static int solution(int a, int b) {
-        int max = a > b ? a : b;
-        int min = a + b - max;
-        
-        for (int i = 1; i <= min; i++) {
-            int num = max * i;
-            if (num % min == 0) {
-                return num;
+        int max = 0;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                max = i;
             }
         }
-        return -1;
+        return (a * b) / max;
     }
 }
