a1fae29b18eafe92da0b86fe639244317aae4092
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-03-25 18:10:49 +0900

    [level 2] Title: N개의 최소공배수, Time: 3.11 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -2,9 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] arr) {
-        if (arr.length == 1) {
-            return arr[0];
-        }
         int max = Arrays.stream(arr).max().getAsInt();
         int count = 1;
         while (true) {
