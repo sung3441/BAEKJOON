e70a78f6a30db5beddc9ccc46b81a590e763ec8e
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-03-26 18:00:02 +0900

    [Silver V] Title: 창문 닫기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -5,18 +5,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int result = 1;
-        int count = 1;
-        int sum = 2;
-        while (true) {
-            if (count <= n && n <= count + sum) {
-                System.out.println(result);
-                break;
-            }
-            result++;
-            count += (sum + 1);
-            sum += 2;
-        }
+        System.out.println((int) Math.sqrt(n));
     }
 }
