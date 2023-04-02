fe43be1463fce695debe692f280446d16ce1b1b1
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-04-02 22:32:20 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        System.out.print(radix(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
+    }
+
+    static String radix(int num, int n) {
+        StringBuilder sb = new StringBuilder();
+        if (num == 0) {
+            return "0";
+        }
+        while (num > 0) {
+            if (num % n < 10) {
+                sb.append((char) (num % n + '0'));
+            } else {
+                sb.append((char) (num % n - 10 + 'A'));
+            }
+            num /= n;
+        }
+        return sb.reverse().toString();
+    }
+}
