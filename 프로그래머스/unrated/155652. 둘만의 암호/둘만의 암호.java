a3e31389431ac1d199a5e64b329764db84b962aa
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-04-18 23:44:07 +0900

    [unrated] Title: 둘만의 암호, Time: 2.74 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -5,13 +5,7 @@ import java.util.stream.IntStream;
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
-        List<Character> skips = skip.chars()
-            .mapToObj(c -> (char) c)
-            .collect(Collectors.toList());
-        List<Character> alphas = IntStream.range(97, 123)
-            .mapToObj(i -> (char) i)
-            .filter(i -> !skips.contains(i))
-            .collect(Collectors.toList());
+        List<Character> alphas = IntStream.range(97, 123).mapToObj(i -> (char) i).filter(i -> skip.indexOf(i) == -1).collect(Collectors.toList());
 
         for (int i = 0; i < s.length(); i++) {
             sb.append(alphas.get((alphas.indexOf(s.charAt(i)) + index) % alphas.size()));
