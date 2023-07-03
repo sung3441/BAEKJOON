d42bdb9fc3dc5649a5b590c6dffe47a492d13889
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-07-03 22:19:26 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,22 +2,21 @@ class Solution {
     public String solution(String s) {
         s = s.toLowerCase();
         char[] chars = s.toCharArray();
-        boolean flag = true;
+        // 65, 97
         
+        boolean flag = true;
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
             if (c == ' ') {
                 flag = true;
                 continue;
             }
-            if (c < 65) {
-                flag = false;
-                continue;
-            }
-            if (flag) {
+            
+            if (c >= 'a' && c <= 'z' && flag) {
                 chars[i] = (char) (c - 32);
-                flag = false;
             }
+            
+            flag = false;
         }
         
         return String.valueOf(chars);
