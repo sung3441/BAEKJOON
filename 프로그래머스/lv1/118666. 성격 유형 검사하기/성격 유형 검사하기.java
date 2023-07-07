a6927061f9899517acff7e7bf96debdb757935e4
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-07-07 15:50:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 14.05 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        char[][] arr = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        int[] score = {3, 2, 1, 0, 1, 2, 3};
+        HashMap<Character, Integer> map = new HashMap();
+        
+        for (int i = 0; i < survey.length; i++) {
+            String s = survey[i];
+            if (choices[i] == 0) {
+                continue;
+            }
+            char target = choices[i] > 4 ? s.charAt(1) : s.charAt(0);
+            map.put(target, map.getOrDefault(target, 0) + score[choices[i] - 1]);
+        }
+        
+        for (char[] a : arr) {
+            int score1 = map.getOrDefault(a[0], 0);
+            int score2 = map.getOrDefault(a[1], 0);
+            
+            answer += score1 - score2 > -1 ? a[0] : a[1];
+        }
+        
+        return answer;
+    }
+}
