13fdd65d4e84bafdfeeb975879b19c960f014aab
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-07-11 13:52:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,17 +1,12 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        boolean hasSameId = false;
         for (String[] loginInfo : db) {
             if (!id_pw[0].equals(loginInfo[0])) {
                 continue;
             }
-            
-            if (id_pw[1].equals(loginInfo[1])) {
-                return "login";
-            }
-            hasSameId = true;
+            return id_pw[1].equals(loginInfo[1]) ? "login" : "wrong pw";
         }
         
-        return hasSameId ? "wrong pw" : "fail";
+        return "fail";
     }
 }
