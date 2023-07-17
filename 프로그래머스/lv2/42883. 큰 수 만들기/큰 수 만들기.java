fd40634be28c61b15028cd6ac81d1c446fd88104
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-07-17 11:59:47 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,49 +2,44 @@ class Solution {
     public String solution(String n, int k) {
         
         StringBuilder number = new StringBuilder();
-        int length = n.length();
-        int maxIndex = 0;
-        char max = (char) 0;
-        
-        for (int i = 0; i <= k; i++) {
-            char c = n.charAt(i);
-            if (c > max) {
-                maxIndex = i;
-                max = c;
-            }
-        }
+        int maxIndex = getMaxNumFirstIndex(n, k);
         
         number.append(n.substring(maxIndex));
-        int count = k - (length - number.length());
+        int count = k - (n.length() - number.length());
         int index = 1;
         
         while (count > 0) {
             boolean flag = true;
             
-            for (int i = index; i < number.length(); i++) {
+            for (int i = index; i < number.length() - 1; i++) {
                 char c = number.charAt(i);
-                if (i == number.length() - 1) {
-                    if (number.charAt(i - 1) > c) {
-                        number.deleteCharAt(i);
-                        flag = false;
-                        index = i - 1;
-                        break;
-                    }
-                } else {
-                    if (number.length() > 2 && number.charAt(i + 1) > c) {
-                        number.deleteCharAt(i);
-                        flag = false;
-                        index = i - 1;
-                        break;
-                    }
+                if (number.length() > 2 && number.charAt(i + 1) > c) {
+                    number.deleteCharAt(i);
+                    flag = false;
+                    index = i - 1;
+                    count--;
+                    break;
                 }
             }
             if (flag) {
                 return number.substring(0, number.length() - count);
             }
-            count--;
         }
         
         return number.toString();
+    }
+    
+    int getMaxNumFirstIndex(String numStr, int k) {
+        int maxIndex = 0;
+        char max = (char) 0;
+        
+        for (int i = 0; i <= k; i++) {
+            char c = numStr.charAt(i);
+            if (c > max) {
+                maxIndex = i;
+                max = c;
+            }
+        }
+        return maxIndex;
     }
 }
