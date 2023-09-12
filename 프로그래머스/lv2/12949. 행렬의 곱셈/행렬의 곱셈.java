b91a005fea3995d95437c334c1179746f3d38136
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-09-12 10:43:55 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.60 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long[][] solution(int[][] arr1, int[][] arr2) {
+        long[][] answer = new long[arr1.length][arr2[0].length];
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
