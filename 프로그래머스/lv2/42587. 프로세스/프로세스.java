80abc04f78c6a4398397f5f1546ab938e92e5f88
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-09-25 18:53:10 +0900

    [level 2] Title: 프로세스, Time: 0.52 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -2,44 +2,21 @@ import java.util.ArrayList;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 1;
-        ArrayList<Process> list = new ArrayList<>();
-        for (int i = 0; i < priorities.length; i++) {
-            list.add(new Process(i, priorities[i]));
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList();
+        for (int priorite : priorities) {
+            list.add(priorite);
         }
-
-        int max = getMax(list);
-        while (!list.isEmpty()) {
-            Process process = list.get(0);
-            list.remove(process);
-            if (max == process.priority) {
-                max = getMax(list);
-                if (process.index == location) {
-                    return answer;
-                }
-                answer++;
-                continue;
-            }
-            list.add(process);
+        
+        while ((list.size) > 0) {
+            
         }
+        
+        
         return answer;
     }
-
-    int getMax(ArrayList<Process> list) {
-        int max = -1;
-        for (Process process : list) {
-            max = Math.max(process.priority, max);
-        }
-        return max;
-    }
-
-    static class Process {
-        int index;
-        int priority;
-
-        public Process(int index, int priority) {
-            this.index = index;
-            this.priority = priority;
-        }
+    
+    boolean isBig() {
+        
     }
 }
