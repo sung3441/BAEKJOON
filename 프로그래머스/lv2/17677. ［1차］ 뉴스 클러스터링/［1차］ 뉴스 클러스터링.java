755155d2e35c5c879bc1f5ff33be41a1e580a84a
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2023-09-26 09:33:55 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.75 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,77 +1,41 @@
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 class Solution {
      public int solution(String str1, String str2) {
         ArrayList<String> list1 = getList(str1);
         ArrayList<String> list2 = getList(str2);
-        Set<String> mergeSet = getMergeSet(list1, list2);
+        ArrayList<String> mergeLIst = new ArrayList<>(list1);
+        mergeLIst.addAll(list2);
+        HashSet<String> mergeSet = new HashSet<>(mergeLIst);
 
         int unionCount = 0;
         int intersectionCount = 0;
-        boolean hasSameData = false;
 
         for (String s : mergeSet) {
-            int count1 = countAndRemove(list1, s);
-            int count2 = countAndRemove(list2, s);
-
-            unionCount += Math.max(count1, count2);
-
+            long count1 = list1.stream().filter(s::equals).count();
+            long count2 = list2.stream().filter(s::equals).count();
+            
+            unionCount += (int) Math.max(count1, count2);
+            
             if (count1 > 0 && count2 > 0) {
-                intersectionCount += Math.min(count1, count2);
+                intersectionCount += (int) Math.min(count1, count2);
             }
         }
 
-        double jaccard = unionCount > 0 ? (double) intersectionCount / unionCount : 1;
-
-        return (int) (jaccard * 65536);
-    }
-
-    int countAndRemove(List<String> list, String target) {
-        int count = 0;
-        for (String s : list) {
-            if (target.equals(s)) {
-                count++;
-            }
-        }
-        remove(list, target, count);
-        return count;
-    }
-
-    void remove(List<String> list, String target, int count) {
-        while (count-- > 0) {
-            list.remove(target);
-        }
-    }
-
-    Set<String> getMergeSet(List<String> list1, List<String> list2) {
-        ArrayList<String> list = new ArrayList<>();
-        list.addAll(list1);
-        list.addAll(list2);
-        return new HashSet<>(list);
+        return (int) ((unionCount > 0 ? (double) intersectionCount / unionCount : 1) * 65536);
     }
 
     ArrayList<String> getList(String str) {
         ArrayList<String> list = new ArrayList<>();
         str = str.toLowerCase();
-
         for (int i = 0; i < str.length() - 1; i++) {
-            String substr = str.substring(i, i + 2);
-            if (valid(substr)) {
-                list.add(substr);
+            String sub = str.substring(i, i + 2);
+            char ch1 = sub.charAt(0), ch2 = sub.charAt(1);
+            if (97 <= ch1 && ch1 <= 122 && 97 <= ch2 && ch2 <= 122) {
+                list.add(sub);
             }
         }
         return list;
-    }
-
-    boolean valid(String str) {
-        for (char c : str.toCharArray()) {
-            if (!(97 <= c && c <= 122)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
