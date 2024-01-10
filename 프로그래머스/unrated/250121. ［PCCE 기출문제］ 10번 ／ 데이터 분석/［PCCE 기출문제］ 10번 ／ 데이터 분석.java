77fb65db284c2257b6f905e8a07dfd66bcb79292
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2024-01-10 14:23:10 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 7.92 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,29 +1,14 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        List<Map<String, Integer>> dataList = new ArrayList<>();
-        for (int[] d : data) {
-            Map<String, Integer> map = new HashMap<>();
-            map.put("code", d[0]);
-            map.put("date", d[1]);
-            map.put("maximum", d[2]);
-            map.put("remain", d[3]);
-            dataList.add(map);
-        }
+        List<String> columnList = Arrays.asList("code", "date", "maximum", "remain");
+        int filterIndex = columnList.indexOf(ext);
+        int sortIndex = columnList.indexOf(sort_by);
 
-        dataList = dataList.stream()
-                .filter(m -> val_ext > m.get(ext))
-                .sorted(Comparator.comparingInt(m -> m.get(sort_by)))
-                .collect(Collectors.toList());
-
-        int[][] result = new int[dataList.size()][data[0].length];
-        for (int i = 0; i < dataList.size(); i++) {
-            Map<String, Integer> map = dataList.get(i);
-            result[i] = new int[]{map.get("code"), map.get("date"), map.get("maximum"), map.get("remain")};
-        }
-
-        return result;
+        return Arrays.stream(data)
+                .filter(o -> val_ext > o[filterIndex])
+                .sorted(Comparator.comparingInt(o -> o[sortIndex]))
+                .toArray(int[][]::new);
     }
 }
