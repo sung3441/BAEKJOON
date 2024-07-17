ee244cac9e72c732125f4fdc3237d7597aecff38
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2024-07-17 23:43:12 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 36.77 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,54 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int result = 0;
+        Map<String, Map<String, Integer>> friendsMap = new HashMap<>();
+        Map<String, Integer[]> presentCountMap = new HashMap<>(); // 0: send, 1: receive
+        for (String friendName : friends) {
+            friendsMap.put(friendName, new HashMap<>());
+            presentCountMap.put(friendName, new Integer[]{0, 0});
+        }
+
+        for (String gift : gifts) {
+            String senderName = gift.split(" ")[0];
+            String receiverName = gift.split(" ")[1];
+
+            Map<String, Integer> sender = friendsMap.get(senderName);
+            sender.put(receiverName, sender.getOrDefault(receiverName, 0) + 1);
+
+            presentCountMap.get(senderName)[0] += 1;
+            presentCountMap.get(receiverName)[1] += 1;
+        }
+
+        // 내가 받을 경우에만 결과 처리
+        for (String senderName : friends) {
+            int presentCount = 0;
+            Map<String, Integer> sender = friendsMap.get(senderName);
+            for (String receiverName : friends) {
+                if (senderName.equals(receiverName)) {
+                    continue;
+                }
+                Map<String, Integer> receiver = friendsMap.get(receiverName);
+                int sendCount = sender.getOrDefault(receiverName, 0);
+                int receiveCount = receiver.getOrDefault(senderName, 0);
+
+                if (receiveCount > sendCount) {
+                    continue;
+                }
+
+                if (sendCount == receiveCount) {
+                    int senderPresentCount = presentCountMap.get(senderName)[0] - presentCountMap.get(senderName)[1];
+                    int receiverPresentCount = presentCountMap.get(receiverName)[0] - presentCountMap.get(receiverName)[1];
+                    if (receiverPresentCount >= senderPresentCount) {
+                        continue;
+                    }
+                }
+                presentCount++;
+            }
+            result = Math.max(result, presentCount);
+        }
+        return result;
+    }
+}
