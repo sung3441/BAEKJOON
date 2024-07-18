39a5e38a7f5ad5e8d78d49d18957b95c9bfe67ce
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2024-07-18 20:20:30 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -3,32 +3,28 @@ import java.util.*;
 public class Solution {
 
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int heelGauge = 0;
-        int maxHealth = health;
-        int attackIndex = 0;
-        for (int i = 0; i <= 1_000; i++) {
-            if (attackIndex == attacks.length) {
-                break;
-            }
-
-            if (attacks[attackIndex][0] == i) {
-                health -= attacks[attackIndex++][1];
-                if (1 > health) {
-                    return -1;
-                }
-                heelGauge = 0;
-                continue;
-            }
+        int maxHealth = health; // 최대 체력
+        int t = bandage[0]; // 시전 시간
+        int x = bandage[1]; // 초당 회복량
+        int y = bandage[2]; // 추가 회복량
+        int lastAttackTime = 0; // 마지막 공격 시간
+        
+        for (int[] attack : attacks) {
+            int attackTime = attack[0]; // 공격 시간
+            int damage = attack[1]; // 피해량
             
-            heelGauge++;
-            int heelAmount = bandage[1];
-            if (heelGauge == bandage[0]) {
-                heelGauge = 0;
-                heelAmount += bandage[2];
+            // 이전 힐량 계산
+            int heelTime = attackTime - lastAttackTime - 1; // 맞은 순간에는 힐 못함
+            int heel = (x * heelTime) + (heelTime / t * y);
+            health = Math.min(health + heel, maxHealth);
+            
+            // 피해량 적용
+            health -= damage;
+            if (health <= 0) {
+                return -1;
             }
-            health = Math.min(health + heelAmount, maxHealth);
+            lastAttackTime = attackTime;
         }
-
         return health;
     }
 }
