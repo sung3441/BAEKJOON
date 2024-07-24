5e286a81b1726d549ef045110e5798264e91d385
SUNGIL CHO <67699933+sung3441@users.noreply.github.com>
2024-07-25 00:20:36 +0900

    [level 1] Title: 시저 암호, Time: 19.18 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -2,21 +2,16 @@ function solution(s, n) {
     let arr = s.split('');
     for (let i = 0; i < arr.length; i++) {
         let alpha = arr[i];
-        arr[i] = alpha === ' ' ? alpha : shift(alpha, n);
+        arr[i] = alpha === ' ' ? alpha : shift(alpha.charCodeAt(), n);
     }
-    
     return arr.join('');
 }
 
 function shift(alpha, num) {
-    alpha = alpha.charCodeAt();
-    let isSmall = false;
-    if (alpha > 90) {
-        alpha -= 32;
-        isSmall = true;
+    let isSmall = alpha > 90;
+    alpha += num;
+    if ((isSmall && alpha > 122) || (!isSmall && alpha > 90)){
+        alpha -= 26;
     }
-    alpha = (alpha + num > 90 ? alpha - 26 : alpha) + num;
-    alpha = String.fromCharCode(alpha);
-    return isSmall ? alpha.toLowerCase() : alpha;
-    
+    return String.fromCharCode(alpha);
 }
